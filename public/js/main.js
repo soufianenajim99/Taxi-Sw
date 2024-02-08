d5b1ed7b2385417b077413f1ae7939e2191b26dc
SoufianeNajim <soufianenajim100@gmail.com>
2024-02-08 11:10:02 +0100

    Home-Page

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const burgerBtn = document.querySelector("#burger-btn");
+const burgerMenu = document.querySelector("#burger-menu");
+
+burgerBtn.addEventListener("click", () => {
+    burgerMenu.classList.toggle("hidden");
+});
+
+console.log("test");
